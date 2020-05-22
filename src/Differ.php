8689e81092067db8d038ecaa9bf6adc11be494cc
fazvil <fazvil@mail.ru>
2020-05-22 14:46:54 +0300

    add comparison tree

--- src/Differ.php
+++ src/Differ.php
@@ -20,14 +20,16 @@ function readFile($file)
     return file_get_contents($file);
 }
 
-function getValue($array, $key)
+function changeValue($value)
 {
-    if (!array_key_exists($key, $array)) {
-        return false;
+    if (is_object($value)) {
+        $vars = get_object_vars($value);
+        foreach ($vars as $key => $val) {
+            return "{\n            {$key}: {$val}\n        }";
+        }
     }
-    $value = $array[$key];
     if (is_bool($value)) {
-        $value = $value ? 'true' : 'false';
+        return $value ? 'true' : 'false';
     }
     return $value;
 }
@@ -35,9 +37,9 @@ function getValue($array, $key)
 function parsers($text, $extension)
 {
     if ($extension === 'json') {
-        $parsed = json_decode($text, true);
+        $parsed = json_decode($text);
     } elseif ($extension === 'yaml') {
-        $parsed = Yaml::parse($text);
+        $parsed = Yaml::parse($text, Yaml::PARSE_OBJECT_FOR_MAP);
     }
     return $parsed;
 }
@@ -55,27 +57,66 @@ function genDiff($pathToFile1, $pathToFile2)
     $parsed1 = parsers($textFromFile1, $extension);
     $parsed2 = parsers($textFromFile2, $extension);
 
-    $keys = Funct\Collection\union(array_keys($parsed1), array_keys($parsed2));
-    $result = array_reduce($keys, function ($acc, $key) use ($parsed1, $parsed2) {
-        $value1 = getValue($parsed1, $key);
-        $value2 = getValue($parsed2, $key);
-        if ($value1) {
-            if ($value2) {
-                if ($value1 === $value2) {
-                    $acc[] = "    {$key}: {$value1}";
-                } else {
-                    $acc[] = "  - {$key}: {$value1}";
-                    $acc[] = "  + {$key}: {$value2}";
-                }
-            } else {
-                $acc[] = "  - {$key}: {$value1}";
+    $ast = function ($object1, $object2) use (&$ast) {
+        $arrayForObject1 = get_object_vars($object1);
+        $arrayForObject2 = get_object_vars($object2);
+
+        $keys1 = array_keys($arrayForObject1);
+        $keys2 = array_keys($arrayForObject2);
+        $jointKeysForTwoArrays = array_values(Funct\Collection\union($keys1, $keys2));
+
+        $map = array_map(function ($key) use ($arrayForObject1, $arrayForObject2, $ast) {
+            $valueFromArray1 = $arrayForObject1[$key] ?? null;
+            $valueFromArray2 = $arrayForObject2[$key] ?? null;
+            if (is_object($valueFromArray1) && is_object($valueFromArray2)) {
+                return [
+                    'key' => $key,
+                    'children' => $ast($valueFromArray1, $valueFromArray2)
+                ];
             }
-        } else {
-            $acc[] = "  + {$key}: {$value2}";
-        }
-        return $acc;
-    }, []);
-    array_unshift($result, "{");
-    $result[] = "}\n";
-    return implode("\n", $result);
+            return [
+                'key' => $key,
+                'value_before' => $valueFromArray1,
+                'value_after' => $valueFromArray2
+            ];
+        }, $jointKeysForTwoArrays);
+        return $map;
+    };
+    $ast = $ast($parsed1, $parsed2);
+
+    $diff = function ($ast) use (&$diff) {
+        $iter = function ($ast, $depth) use (&$iter) {
+            $map = array_map(function ($n) use ($iter, $depth) {
+                $key = $n['key'];
+                $children = $n['children'] ?? null;
+                $spaces = str_repeat(' ', $depth * 4);
+    
+                if ($children) {
+                    $iter = implode("\n", $iter($children, $depth + 1));
+                    return "    {$n['key']}: {\n{$iter}\n    }";
+                }
+                $before = changeValue($n['value_before']);
+                $after = changeValue($n['value_after']);
+    
+                if ($before === $after) {
+                    return "{$spaces}    {$key}: {$before}";
+                } elseif (!$before) {
+                    return "{$spaces}  + {$key}: {$after}";
+                } elseif (!$after) {
+                    return "{$spaces}  - {$key}: {$before}";
+                } else {
+                    return "{$spaces}  - {$key}: {$before}\n{$spaces}  + {$key}: {$after}";
+                }   
+            }, $ast);
+            return $map;
+        };
+        return $iter($ast, 0);
+    };
+
+    $diff = $diff($ast);
+    array_unshift($diff, '{');
+    $diff[] = '}';
+    $diffToString = implode("\n", $diff);
+
+    print_r($diffToString);
 }
